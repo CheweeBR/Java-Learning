1e5304ae14d20eb194d9e36f651ee5fa019b8a58
Tiago Eloy Possidonio <tiagoeloy@alunos.utfpr.edu.br>
2023-04-07 21:40:28 -0300

    Bersion 0.1
    - Criação da classe principal.
    
    - Métodos para realizar os cálculos básicos de Adição, Subtração, Multiplicação e Divisão.
    
    - Apresentação visual via prompt de comando.

---
+++ Calculadora.java
@@ -0,0 +1,80 @@
+// Nome: Tiago Eloy Possidonio Pereira / RA: 2417677
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Calculadora {
+    public static int ad;
+    public static int sub;
+    public static int div;
+    public static int mult;
+
+    public static void main(String args[]) {
+        BufferedReader ler = new BufferedReader(new InputStreamReader(System.in));
+        int alt = 0, n1 = 0, n2 = 0, result = 0;
+        System.out.println("\n Calculadora \n-------------------------");
+        try {
+            System.out.print("Digite o primeiro número: ");
+            n1 = Integer.parseInt(ler.readLine());
+        } catch (Exception e) {
+            System.out.println("Erro na entrada de dados.");
+        }
+        try {
+            System.out.print("Digite o segundo número: ");
+            n2 = Integer.parseInt(ler.readLine());
+        } catch (Exception e) {
+            System.out.println("Erro na entrada de dados.");
+        }
+        System.out.println("\n------------------------- \n Tabela de opções \n-------------------------");
+        System.out.println("1 - Adição (" + n1 + "+" + n2 + ")");
+        System.out.println("2 - Subtração (" + n1 + "-" + n2 + ")");
+        System.out.println("3 - Divisão (" + n1 + "/" + n2 + ")");
+        System.out.println("4 - Multiplicação (" + n1 + "x" + n2 + ")\n-------------------------");
+        try {
+            System.out.print("Alternativa: ");
+            alt = Integer.parseInt(ler.readLine());
+        } catch (Exception e) {
+            System.out.println("Erro na entrada de dados.");
+        }
+        switch (alt) {
+            case 1:
+                result = ad(n1, n2);
+                System.out.println("Resultado: " + n1 + "+" + n2 + " = " + result);
+                break;
+            case 2:
+                result = sub(n1, n2);
+                System.out.println("Resultado: " + n1 + "-" + n2 + " = " + result);
+                break;
+            case 3:
+                result = div(n1, n2);
+                System.out.println("Resultado: " + n1 + "/" + n2 + " = " + result);
+                break;
+            case 4:
+                result = mult(n1, n2);
+                System.out.println("Resultado: " + n1 + "x" + n2 + " = " + result);
+                break;
+            default:
+                System.out.print("O número digitado não está na tabelas de opções.");
+        }
+    }
+
+    public static int ad(int a, int b) {
+        int calc = a + b;
+        return calc;
+    }
+
+    public static int sub(int a, int b) {
+        int calc = a - b;
+        return calc;
+    }
+
+    public static int div(int a, int b) {
+        int calc = a / b;
+        return calc;
+    }
+
+    public static int mult(int a, int b) {
+        int calc = a * b;
+        return calc;
+    }
+}
